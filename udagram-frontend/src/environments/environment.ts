b5038ed3d417680e9c69858d37abad03df4e1d2b
TruongNguyen1203 <nguyentruong.120300@gmail.com>
2023-09-28 00:07:56 +0700

    add screen shot

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a033c79d2053b46a098442ca036e4cb6-431842915.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
