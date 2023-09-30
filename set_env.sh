a338c59f5648d396713e6bbfe4c7619d2bbbdd86
TruongNguyen1203 <nguyentruong.120300@gmail.com>
2023-09-30 16:46:23 +0700

    fix bug

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,8 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=htuPmB2KqBkUWLlbWtOl
-export POSTGRES_HOST=database-1.clsv92aw3sgl.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=V1v22rIDrAyUo3tJPkYm
+export POSTGRES_HOST=database-2.clsv92aw3sgl.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=nguyen-bucket-v2
 export AWS_REGION=us-east-1
